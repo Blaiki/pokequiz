495feffd5295db89d457bcb7c7e9fd732e22b419
Alexander Ganzer <alexanderganzer@MacBook-Pro-Alexander.local>
2017-08-15 08:44:35 +0300

    Added some pictures, Views, GameScene and ChooseGen Scene

---
+++ PokeQuiz/ChooseGenViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChooseGenViewController.swift
+//  PokeQuiz
+//
+//  Created by Alexander Ganzer on 14.08.17.
+//  Copyright © 2017 Alexander Ganzer. All rights reserved.
+//
+
+import UIKit
+
+class ChooseGenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
