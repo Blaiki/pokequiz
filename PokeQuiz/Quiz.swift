a65ba103f53bc3b287d61280adb171dd89f75d8d
iolinpic <iolinpic@gmail.com>
2017-08-16 11:51:34 +0300

    test branch

--- PokeQuiz/Quiz.swift
+++ PokeQuiz/Quiz.swift
@@ -25,6 +25,9 @@ class Quiz: NSObject, NSCoding{
         self.name = name
         super.init()
     }
+    func test(){
+        return "JUST A TEST FUNC"
+    }
     func encode(with aCoder: NSCoder) {
         aCoder.encode(name, forKey: PropertyKey.name)
         aCoder.encode(image, forKey: PropertyKey.image)
