eb625f142fdf6220c79cf94f9fc0a53cbd86d2d7
iolinpic <iolinpic@gmail.com>
2017-08-18 14:39:23 +0300

    working gameplay for 1 currently set image and name ( no random strings)

--- PokeQuiz/GameViewController.swift
+++ PokeQuiz/GameViewController.swift
@@ -18,23 +18,40 @@ class GameViewController: UIViewController {
     @IBOutlet weak var firstRowBtnStack: UIStackView!
     @IBOutlet weak var lastRowBtnStack: UIStackView!
     
+    var startImage:UIImage = UIImage()
+    var shadedImage:UIImage = UIImage()
+    var btnArr = [BtnTagged]()
+    let key:String = "Hello"
+    var str = "Hello,playgrou"
+    var letterBox:[Character] = []
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        var str = "Hello,playgrou"
-        let key:String = "Hello"
-        let letterBox = Array(str.characters)
+        letterBox = Array(str.characters)
         genFirstRow(array: letterBox, color: .red)
         genSecondRow(array: letterBox, color: .red)
         genLabel(length: key.characters.count,color: .red)
-        
-        // Do any additional setup after loading the view.
+        //setup images
+        startImage = imgPoke.image!
+        shadedImage = imageShadow(image: startImage)!
+        imgPoke.image = shadedImage
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func imageShadow(image:UIImage) -> UIImage? {
+        let startImage = CIImage(image:image)
+        if let filter = CIFilter(name: "CIFalseColor") {
+            filter.setValue(startImage, forKey: kCIInputImageKey)
+            filter.setValue(CIColor.black(), forKey: "inputColor0")
+            filter.setValue(CIColor.black(), forKey: "inputColor1")
+            let newImage = UIImage(ciImage: filter.outputImage!)
+            return newImage
+        }
+        return nil
     }
   
     // Adding Sqaures below the Pokemon picture
@@ -45,21 +62,78 @@ class GameViewController: UIViewController {
     
     func buttonAction(sender: UIButton!) {
         let btn: UIButton = sender
-        if btn.tag == 0 {
-            btn.isHidden = true
-        }
-        if btn.tag == 1 {
+        if btn.tag < 20 {
             btn.isEnabled = false
             btn.alpha = 0.0
+            var index:Int = getCurFreeLabel()
+            if(index == -1){
+                clearLabels()
+                index = 0
+            }
+            btnArr[index].thisBtn.setTitle(btn.title(for: .normal), for: .normal)
+            btnArr[index].prevBtn = btn
+            if(checkCorrect()){
+                imgPoke.image = startImage
+            }
         }
-        if btn.tag == 3 {
-            btn.backgroundColor = UIColor.brown
+        if btn.tag >= 20 {
+            if(btn.title(for: .normal) != " "){
+                clearLabel(btn: btn)
+            }
         }
+    }
+    
+    func checkCorrect()->Bool{
+        var tmpStr:String = ""
+        for index in 0 ... btnArr.count-1{
+            if let _:UIButton = btnArr[index].prevBtn{
+                tmpStr += btnArr[index].thisBtn.title(for: .normal)!
+            }
+        }
+        if(tmpStr == key){
+            return true
+        }
+        return false
     }
     
     func genLabel(length:Int,color: UIColor){
         for value in 0 ... length-1{
-            labelStack.addArrangedSubview(btnGenerator(title:" ", bgColor: color, tag: value+20))
+            let btn:UIButton = btnGenerator(title:" ", bgColor: color, tag: value+20)
+            btnArr.append(BtnTagged(thisBtn: btn,prevBtn:nil))
+            labelStack.addArrangedSubview(btn)
+        }
+    }
+    
+    func getCurFreeLabel()->Int{
+        for index in 0 ... btnArr.count-1{
+            guard let _:UIButton = btnArr[index].prevBtn else {
+                return index
+            }
+        }
+        return -1
+    }
+    
+    func clearLabels(){
+        for index in 0 ... btnArr.count-1{
+            if let _:UIButton = btnArr[index].prevBtn{
+                btnArr[index].prevBtn!.isEnabled = true
+                btnArr[index].prevBtn!.alpha = 1.0
+                btnArr[index].prevBtn = nil
+                btnArr[index].thisBtn.setTitle(" ", for: .normal)
+            }
+        }
+    }
+    
+    func clearLabel(btn:UIButton){
+        for index in 0 ... btnArr.count-1{
+            if(btnArr[index].thisBtn.isEqual(btn)){
+                if let _:UIButton = btnArr[index].prevBtn{
+                    btnArr[index].prevBtn!.isEnabled = true
+                    btnArr[index].prevBtn!.alpha = 1.0
+                    btnArr[index].prevBtn = nil
+                    btnArr[index].thisBtn.setTitle(" ", for: .normal)
+                }
+            }
         }
     }
     
@@ -88,4 +162,9 @@ class GameViewController: UIViewController {
         // TODO
         
     }
+}
+
+struct BtnTagged{
+    var thisBtn:UIButton
+    var prevBtn:UIButton?
 }
