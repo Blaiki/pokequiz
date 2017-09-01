7de118edcb5b7a3d394a1929fe65a568c471418d
Alexander Ganzer <alexanderganzer@MacBook-Pro-Alexander.local>
2017-09-01 11:13:09 +0300

    Added statistics and gens

---
+++ PokeQuiz/StatisticViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StatisticViewController.swift
+//  PokeQuiz
+//
+//  Created by Alexander Ganzer on 01.09.17.
+//  Copyright © 2017 Alexander Ganzer. All rights reserved.
+//
+
+import UIKit
+
+class StatisticViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
